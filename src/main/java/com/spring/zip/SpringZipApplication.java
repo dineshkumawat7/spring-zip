02f4c36f4a263bcc3441f64e8159648310c77b01
Dinesh Kumawat <dkumawat7627@gmail.com>
2024-12-19 21:02:45 +0530

    first commit

---
+++ src/main/java/com/spring/zip/SpringZipApplication.java
@@ -0,0 +1,13 @@
+package com.spring.zip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringZipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringZipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/zip/SpringZipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.zip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringZipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
